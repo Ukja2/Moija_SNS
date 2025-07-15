215c908f784a13fa1157550c35b21e3c97a96e23
Ukja2 <jaewooklee739@gmail.com>
2025-07-15 16:18:48 +0900

    fix: firebase 연동 에러 해결

--- moija-sns/src/firebase.js
+++ moija-sns/src/firebase.js
@@ -1,8 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+console.log("🔥 전체 env:", process.env);
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,8 +20,13 @@ const firebaseConfig = {
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app); 
+const db = getFirestore(app); 
 
-export { app, analytics };
+
+
+export { app, analytics, auth, db };

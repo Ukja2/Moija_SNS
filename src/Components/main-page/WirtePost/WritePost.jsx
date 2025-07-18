3dbc8cc106e22e9bae3e4955c74dc2d5121e44b5
Ukja2 <jaewooklee739@gmail.com>
2025-07-19 03:20:00 +0900

    feat: 게시글 작성 시 인증 체크 및 Firestore 위치 정보 파싱 로직 추가

--- src/Components/main-page/WirtePost/WritePost.jsx
+++ src/Components/main-page/WirtePost/WritePost.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { db, auth } from '../../../firebase';
+import { doc, getDoc } from 'firebase/firestore';
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+
 import styles from './WritePost.module.css';
 
+
 function WritePost() {
-  const [user] = useAuthState(auth);
+
+
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  // 로그인 안 했으면 로그인 페이지로 강제 이동
+  useEffect(() => {
+    if (!loading && !user) {
+      alert('로그인한 사용자만 글을 작성할 수 있어요!');
+      navigate('/login');
+    }
+  }, [user, loading, navigate]);
 
   const {
     register,
@@ -18,10 +33,20 @@ function WritePost() {
 
   const onSubmit = async (data) => {
     try {
+      const docRef = doc(db, 'users', user.uid); // firebase에서 현재 사용자 UID로 문서 주소 가져오기
+      const docSnap = await getDoc(docRef); // docRef로부터 실제 데이터를 할당, 서버 통신으로 비동기처리
+
+      let location = '위치정보 없음';
+      if (docSnap.exists()) {
+        const userData = docSnap.data();
+        location = userData.location;
+      }
+
       await addDoc(collection(db, 'posts'), {
         title: data.title,
         content: data.content,
         category: data.category,
+        location: location,
         author: user?.email || '익명',
         createdAt: serverTimestamp(),
       });
@@ -31,6 +56,7 @@ function WritePost() {
       alert('글 작성 중 오류가 발생했습니다: ' + err.message);
     }
   };
+
 
   return (
     <div className={styles.wrapper}>

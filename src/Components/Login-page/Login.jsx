8d1894285490d962d41872006a67a8aee96de3c6
Ukja2 <jaewooklee739@gmail.com>
2025-07-21 04:19:20 +0900

    fix: 로그인 성공 후 홈 화면으로 Nav처리

--- src/Components/Login-page/Login.jsx
+++ src/Components/Login-page/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
     try {
       const userCredential = await signInWithEmailAndPassword(auth, username, password);
       console.log('로그인 성공:', userCredential.user);
-      navigate('/');
+      navigate('/home');
     } catch (error) {
       alert('아이디 또는 비밀번호가 잘못되었습니다.');
     } finally {

9aadfe32085885402f36f4cbaf3a5493cd5a0645
Ukja2 <jaewooklee739@gmail.com>
2025-07-31 14:14:57 +0900

    feat: 모임 페이지 컴포넌트

---
+++ src/Components/Group-page/GroupPage/GroupPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import GroupList from "../GroupList/GroupList";
+import BottomNav from "../../main-page/ButtonNav/BottomNav";
+
+function GroupPage() {
+
+    return (
+        <div>
+            <GroupList />
+            <BottomNav />
+        </div>
+    )
+}
+
+export default GroupPage;

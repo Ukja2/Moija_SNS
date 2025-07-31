375349185537c663105613c822641966c203b600
Ukja2 <jaewooklee739@gmail.com>
2025-07-31 14:37:29 +0900

    chore: 모임 컴포넌트 경로 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import WritePost from './Components/main-page/WirtePost/WritePost.jsx';
 import HomePage from './Components/main-page/HomePage/Homepage.jsx';
 import DetailPost from './Components/main-page/DetailPost/DetailPost.jsx';
 import Calendar from './Components/Calendar-page/Calendar.jsx';
+import GroupPage from './Components/Group-page/GroupPage/GroupPage.jsx';
+import GroupApply from './Components/Group-page/GroupApply/GroupApply.jsx';
+import CreateGroup from './Components/Group-page/CreateGroup/CreateGroup.jsx';
 
 
 function App() {
@@ -17,6 +20,9 @@ function App() {
         <Route path="/home" element={<HomePage />} />
         <Route path="/post/:id" element={<DetailPost />} />
         <Route path="/home/calendar" element={<Calendar />} />
+        <Route path="/home/group" element={<GroupPage />} />
+        <Route path="/home/group/apply/:groupId" element={<GroupApply />} />
+        <Route path="/home/group/create" element={<CreateGroup />} />
       </Routes>
     </Router>
   );

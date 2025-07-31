4ab7cb0f84c12889c4f578db01a2fa90858ec5aa
Ukja2 <jaewooklee739@gmail.com>
2025-07-31 14:14:36 +0900

    chore: 모임 날짜 지정 추가, 모임 생성 시 GroupList 상단에 사용자 지역 표시를 위한 요소 추가

--- src/Components/Group-page/CreateGroup/CreateGroup.jsx
+++ src/Components/Group-page/CreateGroup/CreateGroup.jsx
@@ -2,18 +2,31 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
 import { db, auth } from '../../../firebase';
+import { doc, getDoc } from 'firebase/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import style from './CreateGroup.module.css';
+import { useNavigate } from 'react-router-dom';
 
 function CreateGroup() {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const [user] = useAuthState(auth);
+    const navigate = useNavigate();
 
     const onSubmit = async (data) => {
         if (!user) {
             alert('로그인이 필요해요!');
             return;
         }
+        const userDocRef = doc(db, 'users', user.uid);
+        const userSnap = await getDoc(userDocRef);
+        const userRegion = userSnap.data()?.location;
+
+        if (!userRegion) {
+            // 혹시 모를 에러를 대비해 지역이 설정되어 있지 않은경우 return
+            alert('회원 정보에 지역이 등록되어 있지 않습니다.');
+            return;
+        }
+
 
         try {
             await addDoc(collection(db, 'groups'), {
@@ -21,8 +34,11 @@ function CreateGroup() {
                 creatorId: user.uid,
                 members: [user.uid],
                 createdAt: serverTimestamp(),
+                location: userRegion,
+                date: data.date,
             });
             alert('모임이 성공적으로 생성됐어요!');
+            navigate('/home/group');
         } catch (error) {
             console.error('모임 생성 실패:', error);
         }
@@ -47,6 +63,21 @@ function CreateGroup() {
                     className={style.textarea}
                 />
                 {errors.description && <p className={style.error}>설명도 필수에요</p>}
+
+                <input
+                    type="date"
+                    {...register('date', { required: true })}
+                    className={style.input}
+                    min={new Date().toISOString().split('T')[0]}
+                />
+                {errors.date && <p className={style.error}>날짜를 선택해주세요</p>}
+
+                <input
+                    {...register('meetingplace', { required: true })}
+                    placeholder="모임 장소"
+                    className={style.input}
+                />
+                {errors.meetingplace && <p className={style.error}>위치를 적어주세요</p>}
 
                 <input
                     type="number"

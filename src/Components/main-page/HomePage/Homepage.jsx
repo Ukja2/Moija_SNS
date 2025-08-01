95e6e5acf5a8f110feeca6514a4cb8d525b25afe
Ukja2 <jaewooklee739@gmail.com>
2025-08-01 20:38:29 +0900

    chore: 비로그인시 홈페이지 접근 제한 추가

--- src/Components/main-page/HomePage/Homepage.jsx
+++ src/Components/main-page/HomePage/Homepage.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../../firebase';
+import { Navigate } from 'react-router-dom';
 import PostList from '../PostList/PostList'; 
 import BottomNav from '../ButtonNav/BottomNav'; 
 
 function HomePage() {
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) return <div>로딩 중...</div>;
+  if (!user) return <Navigate to="/" replace />; 
+
   return (
     <div>
       <PostList />
